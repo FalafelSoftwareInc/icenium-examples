41b9b277e7eba2ca6a513333574658c4c8f8398b
J. Tower <jonathantower@gmail.com>
2013-05-08 13:39:12 -0500

    initial commit

--- DataAccessSample/scripts/hello-world.js
+++ DataAccessSample/scripts/hello-world.js
@@ -1,61 +1,30 @@
-// JavaScript Document
 
-// Wait for PhoneGap to load
 document.addEventListener("deviceready", onDeviceReady, false);
 
-// PhoneGap is ready
 function onDeviceReady() {
-    getLocation();
-    navigator.splashscreen.hide();
+	
+    //loadManually();
+    
+	loadWithDatasource();
+    
+	navigator.splashscreen.hide();
 }
 
-function getLocation() {
-    navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError);
+function loadManually() {
+	$.getJSON("http://coupons.staterbros.com/category/index", function (categories) {
+		_(categories).each(function(category) {
+			$('#listView').append('<li><a href="#" class="km-listview-link" data-role="listview-link">' + category.CategoryName + '</a></li>');
+		});
+	});
 }
 
-//=======================Say Hello (Page 1) Operations=======================//
-function sayHello() {
-    var sayHelloInputElem = document.getElementById('helloWorldInput');
-    var sayHelloTextElem = document.getElementById('helloWorldText');
-    var inputText = document.getElementById('txtName');
-
-    sayHelloTextElem.innerHTML = 'Hello, ' + inputText.value + '!';
-    sayHelloTextElem.style.display = 'block';
-    sayHelloInputElem.style.display = 'none';
-}
-
-function sayHelloReset() {
-    var sayHelloInputElem = document.getElementById('helloWorldInput');
-    var sayHelloTextElem = document.getElementById('helloWorldText');
-    var inputText = document.getElementById('txtName');
-
-    inputText.value = '';
-    sayHelloTextElem.style.display = 'none';
-    sayHelloInputElem.style.display = 'block';
-}
-
-//=======================Geolocation Operations=======================//
-// onGeolocationSuccess Geolocation
-function onGeolocationSuccess(position) {
-    // Use Google API to get the location data for the current coordinates
-    var geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    geocoder.geocode({ "latLng": latlng }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            if ((results.length > 1) && results[1]) {
-                $("#myLocation").html(results[1].formatted_address);
-            }
-        }
-    });
-
-    // Use Google API to get a map of the current location
-    // http://maps.googleapis.com/maps/api/staticmap?size=280x300&maptype=hybrid&zoom=16&markers=size:mid%7Ccolor:red%7C42.375022,-71.273729&sensor=true
-    var googleApis_map_Url = 'http://maps.googleapis.com/maps/api/staticmap?size=300x300&maptype=hybrid&zoom=16&sensor=true&markers=size:mid%7Ccolor:red%7C' + latlng;
-    var mapImg = '<img src="' + googleApis_map_Url + '" />';
-    $("#map_canvas").html(mapImg);
-}
-
-// onGeolocationError Callback receives a PositionError object
-function onGeolocationError(error) {
-    $("#myLocation").html("<span class='err'>" + error.message + "</span>");
+function loadWithDatasource() {
+	$.getJSON("http://coupons.staterbros.com/category/index", function (categories) {
+        
+        // gets a reference to the kendo listview object stored in the listView UL
+        var listView = $("#listView").data("kendoMobileListView");
+        
+        // updates the datasource of the listview
+        listView.setDataSource(new kendo.data.DataSource({ data: categories }));
+	});
 }
